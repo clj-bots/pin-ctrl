f3f2d44f2c9b660ce687c378c7dc3913e9e0860e
Christopher Small <metasoarous@gmail.com>
2015-03-23 22:55:54 -0700

    First protocol sketch

--- src/clj_bots/pin_ctrl/protocols.clj
+++ src/clj_bots/pin_ctrl/protocols.clj
@@ -1,3 +1,33 @@
-(ns clj-bots.pin-ctrl.protocols)
+(ns clj-bots.pinctrl.protocols)
+
+(defprotocol PBoard
+  "Basic board protocol, shared by any board, whether on board or over wire."
+  (map-pin [this pinspec]
+    "Some devices, such as the Beaglebone black, have non-trivial mappings between convenient
+    `[header, pin-number]` pairs to the underlying fs-gpio pins.")
+  (init-board! [this config])
+  (get-config [this])
+  (set-config [this]))
+
+(defprotocol POverwireBoard
+  "Overwire boards are boards that run over the wire, like Arduino boards over Firmata. This
+  protocol is for functions specific to these boards."
+  (reset-board! [this] "Overwire boards, such as arduino boards over firmata, can be reset!")
+  (send-message! [this message] "Send an arbitrary message to board.")
+  (get-config! [this] "Load the configuratino settings of the board over the wire directly from the device and cache."))
+
+(defprotocol PPinConfigure
+  (set-mode! [this mode] "Set the mode of the pin, as long as it's supported by the pin's board."))
+
+(defprotocol PReadablePin
+  (read-value [this] "Read the binary or analog value of a pin."))
+
+(defprotocol PWriteablePin
+  (write-value! [this val] "Set the value binary or analog value of a pin."))
+
+(defprotocol PEdgeDetectablePin
+  (set-edge! [this edge] "Set the edge of the pin (:rising, :falling, :both).")
+  (create-edge-channel [this] "Return a channel to be fed edge detection messages.")
+  (release-edge-channel! [this] "Release the edge channel."))
 
 
