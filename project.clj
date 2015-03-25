cd11a987e9bc51a6dd2e51087af5ecf8888a454d
Christopher Small <metasoarous@gmail.com>
2015-03-24 22:52:41 -0700

    Adding peterschwartz' dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [slingshot "0.12.2"]])
+                 [slingshot "0.12.2"]
+                 ; ## Here are dependencies specific to each of the individual implementations
+                 ; rpi dependencies
+                 ; bbb dependencies
+                 ; onboard-common dependencies
+                 [clj-gpio "0.1.0-SNAPSHOT"]
+                 ; firmata dependencies
+                 [clj-firmata "2.0.2-SNAPSHOT"]
+                 ])
