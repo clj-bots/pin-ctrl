75ff2c35c83d48490047f74ccd03f258650486d3
Christopher Small <metasoarous@gmail.com>
2015-03-27 15:50:33 -0700

    Adding core.async to dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [slingshot "0.12.2"]
                  ; ## Here are dependencies specific to each of the individual implementations
                  ; rpi dependencies
