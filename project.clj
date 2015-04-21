22fa7c5123960ab98a4627e7eb20fcb9bee5a937
Christopher T Small <metasoarous@gmail.com>
2015-04-21 02:09:47 -0700

    REbuilt docs and added marginalia to plugins

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-marginalia "0.8.0"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [slingshot "0.12.2"]
